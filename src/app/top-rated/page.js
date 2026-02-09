58551df7504a8b74c643c97e3c17fc85ad58c13e
syariefhidayatt <syariefhidayatt194@gmail.com>
2026-02-09 12:38:07 +0700

    update page

--- src/app/top-rated/page.js
+++ src/app/top-rated/page.js
@@ -1,23 +1,30 @@
 import MovieCard from "@/components/MovieCard";
+import Pagination from "@/components/Pagination";
+import SearchInput from "@/components/SearchInput";
 import Link from "next/link";
 
-async function TopRated() {
+async function TopRated(page = 1) {
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&page=1`
+      `${process.env.NEXT_PUBLIC_BASE_URL}/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_API_KEY}&page=${page}`
     );
     if (!res.ok) {
       throw new Error("Gagal mengambil data")
     }
     const data = await res.json()
-    return data.results;
+    return {
+      movies: data.results,
+      totalPages: data.total_pages
+    }
   } catch (error) {
     console.error("Terjadi kesalahan: ", error)
   }
 }
 
-export default async function TopRatedPage() {
-  const movies = await TopRated()
+export default async function TopRatedPage({ searchParams }) {
+  const params = await searchParams
+  const page = params?.page ? Number(params.page) : 1
+  const { movies, totalPages } = await TopRated(page)
 
   return (
     <div className="container mx-auto p-4">
@@ -27,6 +34,7 @@ export default async function TopRatedPage() {
           <Link href="/" className="mb-4 p-2 hover:bg-rose-700 rounded">Home</Link>
         </div>
       </nav>
+      <SearchInput />
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {movies.map((movie) => (
           movie.poster_path && (
@@ -34,6 +42,7 @@ export default async function TopRatedPage() {
           )
         ))}
       </div>
+      <Pagination page={page} baseUrl="" totalPages={totalPages} />
     </div>
   )
 }
